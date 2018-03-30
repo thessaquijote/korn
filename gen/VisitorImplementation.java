64ce6d0ecc7efa67580ea29581776a5aa0bc3069
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-03-30 13:37:37 +0800

    Added interpreter-intended edits

---
+++ gen/VisitorImplementation.java
@@ -0,0 +1,11 @@
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VisitorImplementation extends KornBaseVisitor<Value> {
+
+
+
+}
